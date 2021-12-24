d560c927561f69c202c4f05fe57af483b7b5fd0a
Severed <92709450+Severed-01@users.noreply.github.com>
2021-12-24 18:54:10 +0530

    Add files via upload

---
+++ commands/vars.js
@@ -0,0 +1,9 @@
+module.exports = ({
+  prefix: "a!", // you can change it with desired prefix
+    xp: "0",
+    level: "0",
+    req: "50",
+    expcd: "45",
+    leveling: "",
+    money: "0"
+  })
